0c0ec95990cff6a2f36edf6442d0aa83f7daceea
ivan <lopatin.ivan28@icloud.com>
2022-07-06 21:44:09 +0300

    add task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе (с помощью рекурсии).
+
+int sumNumbers(int num)
+{
+    if (num < 10)
+        return num;
+    else
+        return (num%10)+sumNumbers(num/10);
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Сумма цифр в введёном числе равняется: " + sumNumbers(number));
