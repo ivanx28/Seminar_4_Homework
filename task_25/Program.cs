76b2f63ecd1f8cc67bf5468e9cb1664ef4aa3a27
ivan <lopatin.ivan28@icloud.com>
2022-07-06 21:43:51 +0300

    add task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double powerNumber(int x, int y)
+{
+    double power = Math.Pow(x, y);
+    return power;
+}
+
+Console.WriteLine("Введите число A и число B");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число A возведённое в степень B равняется: " + powerNumber(numA, numB));
